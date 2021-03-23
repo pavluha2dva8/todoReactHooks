a344229cc5ac088d2e6bf4c0f990447ff3e1ccb7
pavluha2dva8 <pavluha2dva8@gmail.com>
2021-03-23 15:25:59 +0200

    Updates

---
+++ precache-manifest.cf996fa8fe418a27f469551557aaa2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58cad980b9df8a737607",
+    "url": "/todoReactHooks/static/css/main.a90e9ff1.chunk.css"
+  },
+  {
+    "revision": "58cad980b9df8a737607",
+    "url": "/todoReactHooks/static/js/main.f59970b4.chunk.js"
+  },
+  {
+    "revision": "23b28ec6d592c42963b3",
+    "url": "/todoReactHooks/static/js/runtime~main.e5597649.js"
+  },
+  {
+    "revision": "6db5a8a30420870bece0",
+    "url": "/todoReactHooks/static/css/2.ad0f2569.chunk.css"
+  },
+  {
+    "revision": "6db5a8a30420870bece0",
+    "url": "/todoReactHooks/static/js/2.044de0e1.chunk.js"
+  },
+  {
+    "revision": "29b49cb09b29f8111032a1a3e964fefd",
+    "url": "/todoReactHooks/index.html"
+  }
+];
